bd21710b6418242aa813edbd3de0b7bcea72f466
Cauã Teodoro <cauawallacygt@gmail.com>
2025-03-22 18:07:08 -0300

    Classes Certificado, Usuario

--- src/main/java/br/ufrpe/eventos_inscricoes/negocio/beans/Certificado.java
+++ src/main/java/br/ufrpe/eventos_inscricoes/negocio/beans/Certificado.java
@@ -1,4 +1,42 @@
 package br.ufrpe.eventos_inscricoes.negocio.beans;
 
-public class Certificado {
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import lombok.Setter;
+import lombok.Getter;
+
+@Setter
+@Getter
+public class Certificado extends Entidade {
+    private long id;
+    private Usuario usuario;
+    private LocalDateTime dataEmissao;
+
+    public Certificado (long id, Usuario usuario, LocalDateTime dataEmissao) {
+        setId(id);
+        setUsuario(usuario);
+        setDataEmissao(dataEmissao);
+    }
+
+    @Override
+    public String toString() {
+        return "Certificado{" +
+                "id=" + id +
+                ", usuario=" + usuario +
+                ", dataEmissao=" + dataEmissao +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Certificado that = (Certificado) o;
+        return id == that.id && Objects.equals(usuario, that.usuario) && Objects.equals(dataEmissao, that.dataEmissao);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, usuario, dataEmissao);
+    }
 }
--- src/main/java/br/ufrpe/eventos_inscricoes/negocio/beans/Usuario.java
+++ src/main/java/br/ufrpe/eventos_inscricoes/negocio/beans/Usuario.java
@@ -1,4 +1,54 @@
 package br.ufrpe.eventos_inscricoes.negocio.beans;
 
-public class Usuario {
+import java.time.LocalDate;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Usuario extends Entidade {
+    private long id;
+    private String nome;
+    private String email;
+    private String senha;
+    private TipoUsuario tipoUsuario;
+    private LocalDate dataNascimento;
+
+    public Usuario(long id, String nome, String email, String senha, TipoUsuario tipoUsuario, LocalDate dataNascimento) {
+        setId(id);
+        setNome(nome);
+        setEmail(email);
+        setNome(senha);
+        setTipoUsuario(tipoUsuario);
+        setDataNascimento(dataNascimento);
+    }
+
+    public boolean verificarAniversario() {
+        return LocalDate.now().equals(this.dataNascimento);
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                ", tipoUsuario=" + tipoUsuario +
+                ", dataNascimento=" + dataNascimento +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        boolean resultado = false;
+        if (obj instanceof Usuario) {
+            Usuario usuario = (Usuario) obj;
+            if (this.id == usuario.getId() || this.email.equals(usuario.getEmail())) {
+                resultado = true;
+            }
+        }
+        return resultado;
+    }
 }
